e580ab4838af789d20138249b862fdd7f497f99e
James Jackson <james.sorley.jackson@gmail.com>
2023-07-26 15:33:07 -0400

    Better Onboarding

--- Parrotflow/OnboardView.swift
+++ Parrotflow/OnboardView.swift
@@ -10,77 +10,120 @@ import SwiftUI
 struct OnboardView: View {
     
     @Environment(\.openURL) var openURL
+    @State private var needsHelp = false
     
     var body: some View {
-        NavigationView {
-            List {
-                Section {
+        ScrollView(showsIndicators: false) {
+            VStack {
+                Image("onboard-icon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 100)
+                    .padding(.top, 32)
+                VStack(spacing: 10) {
+                    Text("Enable Safari Extension")
+                        .font(.headline)
+                        .foregroundStyle(.secondary)
+                        .fontWeight(.semibold)
+                }
+                .padding()
+            }
+            VStack(spacing: 25) {
+                VStack {
                     HStack {
                         Image(systemName: "1.circle.fill")
-                            .foregroundColor(.red)
-                            .padding(.trailing)
-                        Button {
-                            openURL(URL(string: "App-Prefs:SAFARI&path=WEB_EXTENSIONS/Safari")!)
-                        } label: {
-                            HStack {
-                                Text("Open Safari Extension Settings")
-                                    .foregroundStyle(.white)
-                                    .padding()
-                                Spacer()
-                                Image(systemName: "chevron.right")
-                                    .fontWeight(.bold)
-                                    .foregroundStyle(Color.secondary)
-                            }
-                        }
-                        .padding()
+                            .foregroundColor(.green)
+                        Text("Go to **Settings**")
                     }
-                } header: {
-                    Text("Steps")
+                    Button {
+                        openURL(URL(string: "App-Prefs:SAFARI&path=WEB_EXTENSIONS/Safari")!)
+                    } label: {
+                        HStack {
+                            Text("Open Safari Settings")
+                                .font(.title3)
+                                .foregroundStyle(.white)
+                                .fontWeight(.semibold)
+                                .padding()
+                        }
+                    }
+                    .background {
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                Color(red: 93 / 255, green: 248 / 255, blue: 119 / 255),
+                                Color(red: 20 / 255, green: 194 / 255, blue: 49 / 255)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                    .padding()
                 }
-                Section {
+                VStack {
                     HStack {
                         Image(systemName: "2.circle.fill")
-                            .foregroundColor(.yellow)
-                            .padding(.trailing)
-                        Spacer()
-                        HStack {
-                            Text("Chat with Parrotflow")
-                            Spacer()
-                            HStack {
-                                Text("On")
-                                    .foregroundColor(.yellow)
-                                    .fontWeight(.bold)
-                            }
-                        }
-                        .padding()
-                        .background(Color(uiColor: UIColor.tertiarySystemBackground))
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                            .foregroundColor(.green)
+                        Text("Toggle the extension On")
                     }
+                    Image("onboard-2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 150)
+                }
+                VStack {
                     HStack {
                         Image(systemName: "3.circle.fill")
                             .foregroundColor(.green)
-                            .padding(.trailing)
-                        Spacer()
-                        HStack {
-                            Text("google.com")
-                            Spacer()
-                            HStack {
-                                Text("Allow")
-                                    .foregroundColor(.green)
-                                    .fontWeight(.bold)
-                            }
-                        }
-                        .padding()
-                        .background(Color(uiColor: UIColor.tertiarySystemBackground))
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                        Text("Under Permissions, tap **google.com**")
                     }
-                } header: {
-                    Text("Check Permissions")
-                } footer: {
-                    Text("Once enabled open \"Chat\" from a search result to disable onboarding.")
+                    Image("onboard-3")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 150)
+                }
+                VStack {
+                    HStack {
+                        Image(systemName: "4.circle.fill")
+                            .foregroundColor(.green)
+                        Text("From the list of options, tap Allow")
+                    }
+                    Image("onboard-4")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 150)
+                }
+                VStack {
+                    VStack(spacing: 10) {
+                        HStack {
+                            Image(systemName: "5.circle.fill")
+                                .foregroundColor(.green)
+                            Text("Tap **Chat** from a search result to complete setup and disable onboarding")
+                        }
+                        Button {
+                            openURL(URL(string: "https://www.google.com/search?q=how+tall+is+the+eiffel+tower")!)
+                        } label: {
+                            Text("Make Demo Search…")
+                                .bold()
+                        }
+                    }
+                    Image("onboard-5")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 150)
+                }
+                Divider()
+                VStack {
+                    Button {
+                        needsHelp = true
+                    } label: {
+                        Text("Need Help? Message Support…")
+                    }
                 }
             }
-            .navigationBarTitle("Enable Parrotflow", displayMode: .inline)
+            .padding()
+        }
+        .sheet(isPresented: $needsHelp) {
+            MessageComposer(recipient: "contact@parrotflow.com", message: "I need help enabling Parrotflow")
         }
     }
 }
