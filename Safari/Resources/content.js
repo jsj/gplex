82f61407ec8380fa7cd7c9674f3e479dd55be43f
James Jackson <james.sorley.jackson@gmail.com>
2023-07-27 10:05:59 -0400

    iPad Support

--- Safari/Resources/content.js
+++ Safari/Resources/content.js
@@ -1,34 +1,51 @@
 const hijack = () => {
-    const referenceNode = document.getElementById('hdtb-msb');
-    if (referenceNode) {
-        if (!document.getElementById('parrotflowWidget')) {
-            const newElement = document.createElement('div');
-            newElement.id = "parrotflowWidget";
-            newElement.className = "hdtb-mitem";
-            const urlComponents = document.URL.split("?q=");
-            if (urlComponents.length > 1) {
-                const queryParameter = urlComponents[1];
-                newElement.innerHTML = `<a href='parrotflow://?q=${queryParameter}'><span>Chat</span></a>`;
-            }
-            referenceNode.insertBefore(newElement, referenceNode.firstChild);
-        }
-    } else {
-        if (!document.getElementById('parrotflowWidget')) {
-            const firstBubbleElement = document.querySelector('div[role="listitem"]');
-            if (firstBubbleElement) {
-                const urlComponents = document.URL.split("?q=");
-                if (urlComponents.length > 1) {
-                    const queryParameter = urlComponents[1];
-                    const parentNode = firstBubbleElement.parentNode;
-                    const clonedElement = firstBubbleElement.cloneNode(true);
-                    clonedElement.id = "parrotflowWidget";
-                    const aTag = clonedElement.firstChild;
-                    aTag.removeAttribute('jsaction');
-                    aTag.href = `parrotflow://?q=${queryParameter}`;
-                    const span = aTag.querySelector('span');
-                    span.textContent = "Chat";
-                    parentNode.insertBefore(clonedElement, parentNode.firstChild);
+    const parrotflowWidget = document.getElementById('parrotflowWidget');
+    if (!parrotflowWidget) {
+        try  {
+            // Find Bubble
+            const main = document.getElementById("main");
+            const navigation = main.querySelectorAll('div[role="navigation"]');
+            let a = null;
+            navigation.forEach((navDiv) => {
+                const test = navDiv.querySelector('a');
+                if (test) {
+                    a = test;
+                    return;
                 }
+            });
+            const parent = a.parentNode;
+            
+            // Clone
+            const clone = parent.cloneNode(true);
+            clone.id = "parrotflowWidget"
+            
+            // Reformat
+            const cloneA = clone.querySelector('a');
+            cloneA.removeAttribute('jsaction');
+            cloneA.href = `parrotflow://?q=${document.URL.split("?q=")[1]}`;
+            const cloneSpan = cloneA.querySelector('span');
+            if (cloneSpan) {
+                cloneSpan.textContent = "Chat";
+            } else {
+                const cloneDiv = cloneA.querySelector('div');
+                cloneDiv.textContent = "Chat";
+            }
+            
+            // Append to DOM
+            const container = parent.parentNode;
+            container.insertBefore(clone, container.firstChild);
+            
+        } catch {
+            
+            // Old Style
+            const referenceNode = document.getElementById('hdtb-msb');
+            if (referenceNode) {
+                const newElement = document.createElement('div');
+                newElement.id = "parrotflowWidget";
+                newElement.className = "hdtb-mitem";
+                const queryParameter = document.URL.split("?q=")[1];
+                newElement.innerHTML = `<a href='parrotflow://?q=${queryParameter}'><span>Chat</span></a>`;
+                referenceNode.insertBefore(newElement, referenceNode.firstChild);
             }
         }
     }
