9c79cfa6e722698a77a4cf1d64d78cac9b5fb088
James Jackson <james.sorley.jackson@gmail.com>
2023-08-01 12:06:24 -0400

    macOS Base

---
+++ Mac/MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacApp.swift
+//  Mac
+//
+//  Created by James Jackson on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
